95acf08ec95fb199d7be0dcc61f750dc6d318f5e
Achyut781 <72300422+Achyut781@users.noreply.github.com>
2021-12-07 23:24:20 +0530

    Add files via upload

---
+++ p103.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import plotly.express as px
+df = pd.read_csv('data.csv')
+fig = px.scatter(df, x = 'date', y = 'cases', color = 'country', title = 'Covid cases of famous countries')
+fig.show()
